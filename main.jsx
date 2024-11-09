91b019f35f91fdfe607f21270a9dd08157b83827
void-the-droid <bhuwan626282@gmail.com>
2024-11-09 13:47:27 +0545

    add:page deploy

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import  Base  from './components/Base'
+import  Base  from './src/components/Base'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
